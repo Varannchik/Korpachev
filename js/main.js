e062dabc9d28b81acf68564b14204479898eded9
Varannchik <varann@inbox.ru>
2019-05-07 17:16:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,5 +8,12 @@ $(document).ready(function(){
         $(this).closest('.container').find('svg').toggleClass("rotate un_rotate");
     }); 
 
-    
+    $('.activity_block').click(function(){       
+        $(this).closest('.wrap_activity').find('.content_activity').slideToggle(300);
+        $(this).find('.arrow').addClass(".rotate");
+        $(this).find('.arrow').addClass(".un_rotate");
+        $(this).find('.arrow').toggleClass("rotate un_rotate");
+    });
+
+
 });
